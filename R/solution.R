dd21da9ef8f5f81a328ab5b050ce102aa66a535c
Ayesha Gamage <144423992+ayega233@users.noreply.github.com>
2023-10-10 14:43:42 +0200

    resolve git action

--- R/solution.R
+++ R/solution.R
@@ -3,10 +3,12 @@
 #' @import shiny
 #' @import ggplot2
 #' @import maps
-#' @param county_code A character string representing the county code.
 
 
 #' @export election_summary
+#' @description
+#' Get election result data in sweden in 2022
+#' @return result the parties list and results
 election_summary <- function (){
   election_result <- GET("https://resultat.val.se/assets/valdata/val2022/RD_00_S_mandat.json")
   stop_for_status(election_result,"Summary fetching is not available")
@@ -15,7 +17,12 @@ election_summary <- function (){
   return(result)
 }
 
+#' @title get election result
 #' @export election_result
+#' @description
+#' Get election result data country vice in Sweden in 2022
+#' @param county_code country code
+#' @return result the parties list and results
 election_result <- function (county_code){
   stopifnot("county_code must not empty"= county_code != '')
   url <-paste0("https://resultat.val.se/assets/valdata/val2022/RD_",county_code,"_S_mandat.json")
@@ -27,7 +34,10 @@ election_result <- function (county_code){
   return(result)
 }
 
+#' @title showElectionData
 #' @export showElectionData
+#' @description
+#' show shiny app
 showElectionData <- function(){
   shiny::runApp("R")
 }
